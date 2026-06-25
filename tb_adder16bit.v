d8a4c7283ae388553934b140228045c8005f7f8e
Curumaddi Pranav <curumaddipranav@gmail.com>
2026-06-25 12:25:38 +0530

    16 bit hierarchical adder

--- tb_adder16bit.v
+++ tb_adder16bit.v
@@ -1,5 +1,6 @@
 `include "16bitadder.v"
-module tb_16bitadder ;
+module tb_adder16bit
+ ;
     reg [15:0] a,b;
     reg cin;
     wire [15:0] sum;
@@ -9,8 +10,8 @@ module tb_16bitadder ;
 
     initial
     begin
-        $dumpfile(16bitadder.vcd);
-        $dumpvars(0,tb_16bitadder);
+        $dumpfile(adder16bit.vcd);
+        $dumpvars(0,tb_adder16bit);
         $monitor($time,"a=%h,b=%h,cin=%b,cout=%b,sum=%h,sign=%b,zero=%b,parity=%b,overflow=%b",a,b,cin,cout,sum,sign,zero,parity,overflow);
         #5 a=16'h8fff ; b=16'h8000 ; cin = 0;
         #5 a=16'hfffe ; b=16'h0002 ;
